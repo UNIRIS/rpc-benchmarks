bff5d283cfb024ea3b5aa8de0d8418442b42ca16
Samuel Manzanera <samuel@uniris.io>
2019-08-02 18:51:44 +0200

    remove gen_rpc

--- gen_rcp/lib/gen_rpc/transaction.ex
+++
@@ -1,5 +0,0 @@
-defmodule GenRpc.Transaction do
-  def send_transaction(tx) do
-    tx
-  end
-end
--- gen_rcp/test/gen_rcp_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule GenRcpTest do
-  use ExUnit.Case
-  doctest GenRcp
-
-  test "greets the world" do
-    assert GenRcp.hello() == :world
-  end
-end
